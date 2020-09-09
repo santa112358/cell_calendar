7ff82438e4bbae14bb4c31c87eb0fc4b1eebddbb
santa112358 <sun.jp.0212@gmail.com>
2020-09-10 02:56:42 +0900

    add license

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'cell_calendar example'),
     );
   }
 }
