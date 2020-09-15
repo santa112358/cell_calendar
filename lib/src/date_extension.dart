6a294aa05e0cf12566317bfdad9c2b33ae1993cc
santa112358 <sun.jp.0212@gmail.com>
2020-09-15 19:25:40 +0900

    return monthName by list

--- lib/src/date_extension.dart
+++ lib/src/date_extension.dart
@@ -15,34 +15,21 @@ extension DateExtension on int {
 
   /// MonthName for the number of month
   String get monthName {
-    switch (this) {
-      case 1:
-        return "January";
-      case 2:
-        return "February";
-      case 3:
-        return "March";
-      case 4:
-        return "April";
-      case 5:
-        return "May";
-      case 6:
-        return "June";
-      case 7:
-        return "July";
-      case 8:
-        return "August";
-      case 9:
-        return "September";
-      case 10:
-        return "October";
-      case 11:
-        return "November";
-      case 12:
-        return "December";
-    }
-    assert(false, "UNEXPECTED MONTH");
-    return "";
+    final monthNameList = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
+    return monthNameList[this - 1];
   }
 
   /// Return year and month to be shown by the currentIndex of pageView
